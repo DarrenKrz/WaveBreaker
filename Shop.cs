a03ffc77ab21d97702a0314a1489f1a3ba86ea90
DarrenKrz <[dkrizski@gmail.com]>
2023-05-05 13:16:53 -0600

    added checkbox to indicate ready in shop

--- Shop.cs
+++ Shop.cs
@@ -1,33 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Shop : MonoBehaviour
 {
     public GameObject shopScreen;
     public GameObject P1ReadyButton;
     public GameObject P2ReadyButton;
+    public TMP_Text shopText;
     public bool P1Ready;
     public bool P2Ready;
     public OnGameLoad start;
     public bool inShop;
+    public GameObject P1ReadyToggle;
+    public GameObject P2ReadyToggle;
     void Start() {
         shopScreen.SetActive(false);
         P1ReadyButton.SetActive(false);
         P2ReadyButton.SetActive(false);
+        P1ReadyToggle.SetActive(false);
+        P2ReadyToggle.SetActive(false);
     }
     public void ShowShop() {
         inShop = true;
         shopScreen.SetActive(true);
         P1ReadyButton.SetActive(true);
         P2ReadyButton.SetActive(true);
+        P1ReadyToggle.SetActive(true);
+        P2ReadyToggle.SetActive(true);
     }
     public void SetP1Ready() {
         P1Ready = true;
+        P1ReadyToggle.GetComponent<UnityEngine.UI.Toggle>().isOn = true;
         Ready();
     }
     public void SetP2Ready() {
         P2Ready = true;
+        P2ReadyToggle.GetComponent<UnityEngine.UI.Toggle>().isOn = true;
         Ready();
     }
     public void Ready() {
@@ -36,9 +46,13 @@ public class Shop : MonoBehaviour
             inShop = false;
             P1Ready = false;
             P2Ready = false;
+            P1ReadyToggle.GetComponent<UnityEngine.UI.Toggle>().isOn = false;
+            P2ReadyToggle.GetComponent<UnityEngine.UI.Toggle>().isOn = false;
             shopScreen.SetActive(false);
             P1ReadyButton.SetActive(false);
             P2ReadyButton.SetActive(false);
+            P1ReadyToggle.SetActive(false);
+            P2ReadyToggle.SetActive(false);
         }
     }
 }
